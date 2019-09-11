195aeac567508e08bbdcd2c7edde634cf2375d98
Michael Lawrenson <ctrlcmdspace@gmail.com>
2019-09-11 06:25:06 -0400

    Added help text for celery cli option, ignoring migrations directory

--- src/artifice/scraper/tasks/base.py
+++ src/artifice/scraper/tasks/base.py
@@ -3,4 +3,7 @@ from artifice.scraper.core.app import create_celery_app
 celery = create_celery_app()
 
 def run_celery():
+    """
+    Starts the async task queue
+    """
     celery.worker_main(['', '-B'])
