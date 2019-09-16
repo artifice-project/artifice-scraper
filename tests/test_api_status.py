ce041b0168d0e0a4e89ea262632605cf69a5333c
Michael Lawrenson <ctrlcmdspace@gmail.com>
2019-09-16 08:39:50 -0400

    woohoo test coverage at 92 percent

--- tests/test_api_status.py
+++ tests/test_api_status.py
@@ -19,17 +19,49 @@ def test_post_missing_body_status(client, session):
     assert 'error' in response.json
 
 
-def test_post_should_change_status(client, session):
-    enabled = True
-    debug = True
-    polite = 999
-    json_data = dict(enabled=enabled, debug=debug, polite=polite)
+def test_post_should_change_enabled_status(client, session):
+    json_data = {'enabled': True}
     response = client.post(endpoint, json=json_data)
 
     assert response.status_code == 200
-    assert response.json['enabled'] == enabled
-    assert response.json['debug'] == debug
-    assert response.json['polite'] == polite
+    assert response.json['enabled'] == True
+
+    json_data = {'enabled': False}
+    response = client.post(endpoint, json=json_data)
+
+    assert response.status_code == 200
+    assert response.json['enabled'] == False
+
+
+
+
+def test_post_should_change_debug_status(client, session):
+    json_data = {'debug': True}
+    response = client.post(endpoint, json=json_data)
+
+    assert response.status_code == 200
+    assert response.json['debug'] == True
+
+    json_data = {'debug': False}
+    response = client.post(endpoint, json=json_data)
+
+    assert response.status_code == 200
+    assert response.json['debug'] == False
+
+
+def test_post_should_change_polite_status(client, session):
+
+    json_data = {'polite': 42}
+    response = client.post(endpoint, json=json_data)
+
+    assert response.status_code == 200
+    assert response.json['polite'] == 42
+
+    json_data = {'polite': 2}
+    response = client.post(endpoint, json=json_data)
+
+    assert response.status_code == 200
+    assert response.json['polite'] == 2
 
 
 def test_post_should_raise_error_status(client, session):
