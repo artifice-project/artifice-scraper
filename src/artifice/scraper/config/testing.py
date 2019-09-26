aa31dcfe1bf3ae08f8f4b568d8a62a61d9121ec3
Michael Lawrenson <ctrlcmdspace@gmail.com>
2019-09-26 01:06:22 -0400

    adding declarations to testing config

--- src/artifice/scraper/config/testing.py
+++ src/artifice/scraper/config/testing.py
@@ -5,5 +5,7 @@ DEBUG = True
 TESTING = True
 SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/artifice_scraper_test'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+CELERY_BROKER_URL = 'postgresql://localhost/artifice_scraper_test'
+CELERY_INCLUDE = ['artifice.scraper.tasks']
+CELERY_BACKEND = 'rpc://'
 ERROR_404_HELP = False
