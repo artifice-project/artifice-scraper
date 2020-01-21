757ec9e17d3bb63eda6ecae2c430bce6a84cf4ce
Michael Lawrenson <ctrlcmdspace@gmail.com>
2020-01-20 23:35:57 -0500

    reconfigured logging within a task component

--- src/artifice/scraper/tasks/scheduled_tasks.py
+++ src/artifice/scraper/tasks/scheduled_tasks.py
@@ -1,11 +1,13 @@
 from artifice.scraper.tasks.base import celery_app
 
+import logging
+log = logging.getLogger(__name__)
 
 @celery_app.task(name='tasks.scheduled_task')
 def scheduled_task():
     from flask import current_app
     env = current_app.config.get('ENV', '<missing>')
-    print(' * Heartbeat (config:{0})'.format(env))
+    log.debug(' * Heartbeat (config:{0})'.format(env))
 
 
 # @celery_app.task(name='tasks.health_check')
