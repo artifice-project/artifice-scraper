643c0c0dfb439dec39f92e6c0e0370ed0ef328a9
Michael Lawrenson <ctrlcmdspace@gmail.com>
2020-01-20 23:35:28 -0500

    moved supervisor_schema inside the Supervisor object to eliminate the need to call a load on the status() function call

--- src/artifice/scraper/resources/queue.py
+++ src/artifice/scraper/resources/queue.py
@@ -66,19 +66,19 @@ class QueueResource(Resource):
             for each in data:
                 result = db.session.query(Queue).filter_by(url=each.url).first()
                 if not result:
-                    log.info(' * RECEIVED {0}'.format(each.url))
+                    log.info('[RECEIVED] {0}'.format(each.url))
                     db.session.add(each)
                     reply.append(queue_task_schema.dump(each).data)
                 elif result:
-                    log.debug(' * DUPLICATE: {0}'.format(each.url))
+                    log.debug('[DUPLICATE]: {0}'.format(each.url))
                     pass
             db.session.commit()
             for each in reply:
-                if not Supervisor.status().get('debug'):
+                if not Supervisor.status(key='debug'):
                     send_to_celery(each['url'])
-                    log.info(' * TASKED {0}'.format(each['url']))
+                    log.info('[TASKED] {0}'.format(each['url']))
                 else:
-                    log.info(' * DEBUG MODE ENABLED {0}'.format(each['url']))
+                    log.info('[DEBUG MODE ENABLED] {0}'.format(each['url']))
             return reply_success(reply)
         return reply_empty()
 
@@ -93,12 +93,12 @@ class QueueResource(Resource):
             log.error({__class__: errors})
             return reply_error(errors)
         elif data:
-            log.debug(' * RETURNING {0}'.format(queue_schema.dump(data)))
+            log.debug('[RETURNING] {0}'.format(queue_schema.dump(data)))
             result = db.session.query(Queue).filter_by(url=data.url).first()
             if result:
                 result.status = data.status
             else:
-                log.error(' * UNEXPECTED {0}'.format(queue_schema.dump(data)))
+                log.warning('[UNEXPECTED] {0}'.format(queue_schema.dump(data)))
                 db.session.add(data)
             db.session.commit()
             reply, _ = queue_schema.dump(result)
